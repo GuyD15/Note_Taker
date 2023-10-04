29ad4fdda06be619f5cfc9fc4bd10e0d98d1bc79
Guy Domino <guywayne.domino7@gmail.com>
2023-10-04 12:15:14 -0500

    Added routes to allow notes returning to html files

---
+++ routes/home-routes.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = (app) => {
+//  return the notes.html file.
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+  });
+  // return the index.html file.
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  })
+};
